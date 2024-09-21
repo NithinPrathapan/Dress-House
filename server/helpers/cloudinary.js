4d42b35817b50428246022c3761628234a235194
Nithin Prathapan <nithinprathapan369@gmail.com>
2024-09-21 20:17:32 +0530

    cloudinaryconfig

---
+++ server/helpers/cloudinary.js
@@ -0,0 +1,18 @@
+import { v2 as cloudinary } from "cloudinary";
+import multer from "multer";
+
+cloudinary.config({
+  cloud_name: "dgsyuonxp",
+  api_key: "414462944245925",
+  api_secret: "nm5vgkdx4-LynAaKPsAciW9OQUc",
+});
+
+const storage = new multer.memoryStorage();
+export const handleImageUpload = async () => {
+  const result = await cloudinary.uploader.upload(file, {
+    resource_type: "auto",
+  });
+  return result;
+};
+
+export const upload = multer({ storage });
